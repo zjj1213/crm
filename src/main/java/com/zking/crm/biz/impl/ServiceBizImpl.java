458a08da3404fdd710d7a6e7d0986b1790388cdd
quanchanghui <2817442243@qq.com>
2018-01-26 19:04:35 +0800

    qch

--- src/main/java/com/zking/crm/biz/impl/ServiceBizImpl.java
+++ src/main/java/com/zking/crm/biz/impl/ServiceBizImpl.java
@@ -6,7 +6,7 @@ import com.zking.crm.model.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @org.springframework.stereotype.Service
-public class ServiceImpl implements IService {
+public class ServiceBizImpl implements IService {
     @Autowired
     private ServiceMapper serviceMapper;
     @Override
