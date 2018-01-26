9855e66e14f26756bf0bf4af05e6bdce8dc8815a
quanchanghui <2817442243@qq.com>
2018-01-26 18:43:52 +0800

    qch

---
+++ src/main/java/com/zking/crm/biz/IService.java
@@ -0,0 +1,25 @@
+package com.zking.crm.biz;
+
+import com.zking.crm.model.Service;
+
+public interface IService {
+    void addService(Service service);
+
+    void delService(Service service);
+
+    void editZhipai(Service service);
+
+    void doFuwuchuli(Service service);
+
+    void doFuwufangkui(Service service);
+
+    Service loadFuwuguidang(Long svrId);
+
+    Service listStatus1();
+
+    Service listStatus2();
+
+    Service listStatus3();
+
+    Service listStatus4();
+}
