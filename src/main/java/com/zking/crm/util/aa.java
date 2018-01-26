9f220a456d2c6e5a334bf283dd94bd2bff769d24
lingxi123 <1781433975@qq.com>
2018-01-26 17:01:30 +0800

    qwerty

--- src/main/java/com/zking/crm/util/aa.java
+++ src/main/java/com/zking/crm/util/aa.java
@@ -9,4 +9,9 @@ public class aa {
     public void saa() {
 
     }
+
+
+    public void saa2() {
+
+    }
 }
---
+++ src/main/java/com/zking/crm/util/vvv.java
@@ -0,0 +1,8 @@
+package com.zking.crm.util;
+
+public class vvv {
+
+    public static void main(String[] args) {
+        System.out.println("1234");
+    }
+}
