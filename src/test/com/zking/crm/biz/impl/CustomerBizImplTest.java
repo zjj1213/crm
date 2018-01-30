f6bd928834b967ada6e678f6af314006f3aa4f9c
zjj1213 <2206455153@qq.com>
2018-01-30 16:58:23 +0800

    zjj

--- src/test/com/zking/crm/biz/impl/CustomerBizImplTest.java
+++ src/test/com/zking/crm/biz/impl/CustomerBizImplTest.java
@@ -2,10 +2,12 @@ package com.zking.crm.biz.impl;
 
 import com.zking.crm.biz.ICustomerBiz;
 import com.zking.crm.model.Customer;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.sound.midi.Soundbank;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -36,6 +38,24 @@ public class CustomerBizImplTest extends BaseTestCase{
          }
 
     }
+
+    @Test
+    public void load() throws Exception {
+        customer.setCustNo("KH_20180127_00002");
+        customer.setCustStatus("1");
+        Customer loadlist = customerBiz.load(customer);
+        System.out.println(loadlist);
+    }
+
+
+    @Test
+    public void editCustomer() throws Exception {
+      customer.setCustAddr("上海黄浦区");
+      customer.setCustNo("KH_20180126_00001");
+      customerBiz.editCustomer(customer);
+    }
+
+
 
 
 }
